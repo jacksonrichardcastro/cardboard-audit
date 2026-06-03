fef1088d45bade6f93ab0f10035e58af348f88dc
jacksonrichardcastro <jacksoncastrosmacbook@gmail.com>
2026-06-03 08:54:35 -0500

    fix: add missing profile_setup_completed to test DB migration

--- src/lib/db/migrations/0010_missing_cols.sql
+++ src/lib/db/migrations/0010_missing_cols.sql
@@ -1,1 +1,2 @@
 ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "welcome_modal_dismissed" boolean DEFAULT false NOT NULL;
+ALTER TABLE "profiles" ADD COLUMN IF NOT EXISTS "profile_setup_completed" boolean DEFAULT false NOT NULL;
